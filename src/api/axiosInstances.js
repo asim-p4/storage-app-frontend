1755230069d18275c5ab9a66277c861adbd56140
asim-p4 <asim00silent99@gmail.com>
2026-05-21 16:47:01 +0500

    remove hardcode uri in page

--- src/api/axiosInstances.js
+++ src/api/axiosInstances.js
@@ -1,12 +1,10 @@
 import axios from "axios";
 
-const BASE_URL = "https://smart-storage-sys.onrender.com";
-
 export const axiosWithCreds = axios.create({
-  baseURL: BASE_URL,
+  baseURL: import.meta.env.BASE_URL,
   withCredentials: true,
 });
 
 export const axiosWithoutCreds = axios.create({
-  baseURL: BASE_URL,
+  baseURL: import.meta.env.BASE_URL,
 });
