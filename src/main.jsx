b0eb61de0ffd3fd7cfad16790d6cce5a6fff9b23
asim-p4 <asim00silent99@gmail.com>
2026-06-04 12:22:41 +0500

    add log

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,12 @@ import App from "./App.jsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+console.log("clientid", clientId);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <GoogleOAuthProvider clientId={clientId}>
       <App />
     </GoogleOAuthProvider>
-  </StrictMode>
+  </StrictMode>,
 );
