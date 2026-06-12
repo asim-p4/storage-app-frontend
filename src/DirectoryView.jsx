66bb9078caae6fbdcf184f6871b48e9dfaed9efc
asim-p4 <asim00silent99@gmail.com>
2026-06-12 23:20:09 +0500

    drive name

--- src/DirectoryView.jsx
+++ src/DirectoryView.jsx
@@ -26,7 +26,7 @@ function DirectoryView() {
   const { dirId } = useParams();
   const navigate = useNavigate();
 
-  const [directoryName, setDirectoryName] = useState("FAST ");
+  const [directoryName, setDirectoryName] = useState("FAST");
   const [directoriesList, setDirectoriesList] = useState([]);
   const [filesList, setFilesList] = useState([]);
   const [errorMessage, setErrorMessage] = useState("");
