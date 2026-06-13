5e5083e4334e797b3b67399082862e08d990d512
asim-p4 <asim00silent99@gmail.com>
2026-06-13 14:43:16 +0500

    drive name

--- src/DirectoryView.jsx
+++ src/DirectoryView.jsx
@@ -26,7 +26,7 @@ function DirectoryView() {
   const { dirId } = useParams();
   const navigate = useNavigate();
 
-  const [directoryName, setDirectoryName] = useState("QWEty");
+  const [directoryName, setDirectoryName] = useState("QE");
   const [directoriesList, setDirectoriesList] = useState([]);
   const [filesList, setFilesList] = useState([]);
   const [errorMessage, setErrorMessage] = useState("");
