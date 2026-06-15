f8b26fdf6fed725ba1aa3d5a9a512b4a0028d917
asim-p4 <asim00silent99@gmail.com>
2026-06-15 12:55:50 +0500

    drive name

--- src/DirectoryView.jsx
+++ src/DirectoryView.jsx
@@ -26,7 +26,7 @@ function DirectoryView() {
   const { dirId } = useParams();
   const navigate = useNavigate();
 
-  const [directoryName, setDirectoryName] = useState("CLEAN");
+  const [directoryName, setDirectoryName] = useState("SPAWN");
   const [directoriesList, setDirectoriesList] = useState([]);
   const [filesList, setFilesList] = useState([]);
   const [errorMessage, setErrorMessage] = useState("");
@@ -53,7 +53,7 @@ function DirectoryView() {
   const loadDirectory = async () => {
     try {
       const data = await getDirectoryItems(dirId);
-      setDirectoryName(dirId ? data.name : "CLEAN");
+      setDirectoryName(dirId ? data.name : "SPAWN");
       setDirectoriesList([...data.directories].reverse());
       setFilesList([...data.files].reverse());
     } catch (err) {
