b67cdf7d92b63cdceff6c1431e069ec39f485b42
asim-p4 <asim00silent99@gmail.com>
2026-06-16 09:08:32 +0500

    all

--- src/DirectoryView.jsx
+++ src/DirectoryView.jsx
@@ -26,7 +26,7 @@ function DirectoryView() {
   const { dirId } = useParams();
   const navigate = useNavigate();
 
-  const [directoryName, setDirectoryName] = useState("SLOP");
+  const [directoryName, setDirectoryName] = useState("SLP");
   const [directoriesList, setDirectoriesList] = useState([]);
   const [filesList, setFilesList] = useState([]);
   const [errorMessage, setErrorMessage] = useState("");
