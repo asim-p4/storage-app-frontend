c00d8215ce8288c0d1e86ce09ce3ffde53fe4a25
asim-p4 <asim00silent99@gmail.com>
2026-06-19 16:40:41 +0500

    client main

--- src/DirectoryView.jsx
+++ src/DirectoryView.jsx
@@ -53,7 +53,7 @@ function DirectoryView() {
   const loadDirectory = async () => {
     try {
       const data = await getDirectoryItems(dirId);
-      setDirectoryName(dirId ? data.name : "dois");
+      setDirectoryName(dirId ? data.name : "dois") ;
       setDirectoriesList([...data.directories].reverse());
       setFilesList([...data.files].reverse());
     } catch (err) {
